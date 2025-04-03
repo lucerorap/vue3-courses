547763256bcd9280a61f618dbbd93b83c387d025
Adrian Rafael Lucero <adrianrafaellucero9@gmail.com>
2025-04-03 22:41:06 +0800

    component variants

--- medium-projects/event-booking/src/components/BookingItem.vue
+++ medium-projects/event-booking/src/components/BookingItem.vue
@@ -1,7 +1,13 @@
 <template>
-  <SectionCard> Booking to Vue Conference</SectionCard>
+  <SectionCard>
+    <div class="flex justify-between">
+      <div>Booking to Vue Conference</div>
+      <RoundedButton variant="danger">Cancel</RoundedButton>
+    </div>
+  </SectionCard>
 </template>
 
 <script setup>
 import SectionCard from '@/components/SectionCard.vue'
+import RoundedButton from '@/components/RoundedButton.vue'
 </script>
