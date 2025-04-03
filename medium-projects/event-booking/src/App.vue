cdd6fa8af6897a682a2f96a40cff7e8d26a05c6e
Adrian Rafael Lucero <adrianrafaellucero9@gmail.com>
2025-04-03 14:36:46 +0800

    Event Card Component

--- medium-projects/event-booking/src/App.vue
+++ medium-projects/event-booking/src/App.vue
@@ -1,3 +1,20 @@
 <template>
-  <main>Hello!</main>
+  <main class="container mx-auto my-8 space-y-8">
+    <h1 class="text-4xl">Event Booking App</h1>
+    <h2 class="text-2xl font-medium">All Events</h2>
+    <section class="grid grid-cols-2 gap-8">
+      <EventCard
+        v-for="i in 8"
+        :key="i"
+        title="Vue Conference 2024"
+        when="2024-05-01"
+        description="Conference about Vue and JavaScript"
+      />
+    </section>
+    <h2 class="text-2xl font-medium">Your Bookings</h2>
+  </main>
 </template>
+
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+</script>
