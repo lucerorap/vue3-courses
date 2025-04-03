ac32470597e731d0179422b62f3fc9bb54c3bf3f
Adrian Rafael Lucero <adrianrafaellucero9@gmail.com>
2025-04-03 22:30:46 +0800

    named slots

--- medium-projects/event-booking/src/components/EventCard.vue
+++ medium-projects/event-booking/src/components/EventCard.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="border border-gray-800 bg-white rounded-md">
-    <h3 class="p-4 font-medium text-xl border-b border-gray-200">{{ title }}</h3>
-    <p class="p-4 border-b border-gray-200">{{ when }}</p>
-    <p class="p-4 border-b border-gray-200">{{ description }}</p>
-    <section class="flex justify-end p-4">
-      <RoundButton @click="$emit('register')">Register</RoundButton>
-    </section>
-  </div>
+  <SectionCard>
+    <template #header>
+      {{ title }}
+    </template>
+    {{ when }}
+    <template #footer>
+      <div class="space-y-4">
+        <p class="">{{ description }}</p>
+        <section class="flex justify-end">
+          <RoundedButton @click="$emit('register')">Register</RoundedButton>
+        </section>
+      </div>
+    </template>
+  </SectionCard>
 </template>
 
 <script setup>
-import Button from '@/components/RoundButton.vue'
+import RoundedButton from '@/components/RoundedButton.vue'
+import SectionCard from '@/components/SectionCard.vue'
 
 defineProps({
   title: String,
---
+++ medium-projects/event-booking/src/components/SectionCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <RoundedCard>
+    <h3 class="p-4 font-medium text-xl border-b border-gray-200"><slot name="header"></slot></h3>
+    <p class="p-4 border-b border-gray-200"><slot></slot></p>
+    <div class="p-4">
+      <slot name="footer"></slot>
+    </div>
+  </RoundedCard>
+</template>
+
+<script setup>
+import RoundedCard from '@/components/RoundedCard.vue'
+</script>
