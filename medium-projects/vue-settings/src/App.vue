de51e894e95444aff250fec8fd6f8f319af30d28
Adrian Rafael Lucero <adrianrafaellucero9@gmail.com>
2025-04-05 12:06:20 +0800

    Vue Settings: Rendering Tab Content using Dynamic Components

--- medium-projects/vue-settings/src/App.vue
+++ medium-projects/vue-settings/src/App.vue
@@ -7,28 +7,38 @@
         </li>
       </ul>
     </nav>
+    <component :is="currentTabComponent" />
   </main>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import type { Tab, TabKey } from '@/types';
 import TabLink from '@/components/TabLink.vue';
+import GeneralSettings from '@/components/GeneralSettings.vue';
+import NotificationSettings from '@/components/NotificationSettings.vue';
+import PrivacySettings from '@/components/PrivacySettings.vue';
 
 const tabs: Tab[] = [
   {
     key: 'General',
-    label: ' General',
+    label: 'General',
+    component: GeneralSettings,
   },
   {
     key: 'Notifications',
-    label: ' Notifications',
+    label: 'Notifications',
+    component: NotificationSettings,
   },
   {
     key: 'Privacy',
-    label: ' Privacy',
+    label: 'Privacy',
+    component: PrivacySettings,
   },
 ];
 
 const currentTab = ref<TabKey>('General');
+const currentTabComponent = computed(
+  () => tabs.find((tab) => tab.key === currentTab.value)?.component,
+);
 </script>
