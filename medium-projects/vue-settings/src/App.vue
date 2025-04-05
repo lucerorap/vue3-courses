c375d4f2bcb87710b162e868efbbe69773fad8e9
Adrian Rafael Lucero <adrianrafaellucero9@gmail.com>
2025-04-05 11:51:46 +0800

    Vue Settings: Rendering Tab COntent using Dynamic Components

--- medium-projects/vue-settings/src/App.vue
+++ medium-projects/vue-settings/src/App.vue
@@ -1,6 +1,19 @@
+<template>
+  <main class="max-w-2xl mx-auto px-4">
+    <nav class="font-medium text-center text-gray-500 border-b border-gray-200 mb-4">
+      <ul class="flex flex-wrap -mb-px">
+        <li v-for="tab in tabs" :key="tab.key">
+          <TabLink :tab="tab" :current-tab="currentTab" @click="currentTab = tab.key" />
+        </li>
+      </ul>
+    </nav>
+  </main>
+</template>
+
 <script setup lang="ts">
 import { ref } from 'vue';
 import type { Tab, TabKey } from '@/types';
+import TabLink from '@/components/TabLink.vue';
 
 const tabs: Tab[] = [
   {
@@ -19,7 +32,3 @@ const tabs: Tab[] = [
 
 const currentTab = ref<TabKey>('General');
 </script>
-
-<template>
-  <main>Helloworld</main>
-</template>
--- medium-projects/vue-settings/src/components/TabLink.vue
+++ medium-projects/vue-settings/src/components/TabLink.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>Link</div>
+  <button
+    :class="{
+      'p-4 border-b-2': true,
+      'border-transparent': !isActive,
+      'border-b-blue-600 text-blue-600': isActive,
+    }"
+    @click="$emit('click')"
+  >
+    {{ tab.label }}
+  </button>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import type { Tab } from '@/types';
 
-defineProps<{
+const props = defineProps<{
   currentTab: string;
   tab: Tab;
 }>();
@@ -13,4 +23,6 @@ defineProps<{
 defineEmits<{
   click: [];
 }>();
+
+const isActive = computed(() => props.currentTab === props.tab.key);
 </script>
