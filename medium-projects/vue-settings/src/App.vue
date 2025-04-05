ee4791ecc8d347bda0ba6103c7f95fe35e375caa
Adrian Rafael Lucero <adrianrafaellucero9@gmail.com>
2025-04-05 11:40:31 +0800

    Vue Settings: defineprops and events with typescript types & Types of refs and other data

--- medium-projects/vue-settings/src/App.vue
+++ medium-projects/vue-settings/src/App.vue
@@ -1,4 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { Tab, TabKey } from '@/types';
+
+const tabs: Tab[] = [
+  {
+    key: 'General',
+    label: ' General',
+  },
+  {
+    key: 'Notifications',
+    label: ' Notifications',
+  },
+  {
+    key: 'Privacy',
+    label: ' Privacy',
+  },
+];
+
+const currentTab = ref<TabKey>('General');
+</script>
 
 <template>
   <main>Helloworld</main>
---
+++ medium-projects/vue-settings/src/components/TabLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Link</div>
+</template>
+
+<script setup lang="ts">
+import type { Tab } from '@/types';
+
+defineProps<{
+  currentTab: string;
+  tab: Tab;
+}>();
+
+defineEmits<{
+  click: [];
+}>();
+</script>
