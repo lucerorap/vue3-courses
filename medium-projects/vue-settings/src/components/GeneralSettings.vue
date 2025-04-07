95c4be7c2111c7b4d7b46663c3d9526bba3f55a8
Adrian Rafael Lucero <adrianrafaellucero9@gmail.com>
2025-04-07 12:47:21 +0800

    Vue Settings: General setting form & useSetting composable

--- medium-projects/vue-settings/src/components/GeneralSettings.vue
+++ medium-projects/vue-settings/src/components/GeneralSettings.vue
@@ -1,1 +1,51 @@
-<template><div>General</div></template>
+<template>
+  <div>
+    <h2 class="text-2xl mb-4">General</h2>
+    <form class="space-y-4 mx-auto">
+      <div>
+        <label>Username</label>
+        <input type="text" name="username" v-model="general.username" />
+      </div>
+      <div>
+        <label>Email</label>
+        <input type="email" name="email" v-model="general.email" />
+      </div>
+      <div>
+        <label>About Me</label>
+        <textarea v-model="general.about"> </textarea>
+      </div>
+      <div>
+        <label>Gender</label>
+        <label>
+          <input type="radio" name="gender" value="male" v-model="general.gender" />
+          <span>Male</span>
+        </label>
+        <label>
+          <input type="radio" name="gender" value="female" v-model="general.gender" />
+          <span>Female</span>
+        </label>
+        <label>
+          <input type="radio" name="gender" value="other" v-model="general.gender" />
+          <span>Other</span>
+        </label>
+      </div>
+      <div>
+        <label>Country</label>
+        <select name="country" v-model="general.country">
+          <option>USA</option>
+          <option>Canada</option>
+          <option>UN</option>
+        </select>
+      </div>
+      <button type="submit" class="btn-primary">Save</button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useSettings } from '@/composables/useSettings';
+
+const { general } = useSettings();
+const thisWIllAlwaysBe0 = ref(0);
+</script>
