2aebd5f9e26e6b7a0f2ad3d107e323273b043735
Adrian Rafael Lucero <adrianrafaellucero9@gmail.com>
2025-04-07 13:37:26 +0800

    Vue Settings: useNotification composable

---
+++ medium-projects/vue-settings/src/composables/useNotifications.ts
@@ -0,0 +1,29 @@
+import { ref } from 'vue';
+
+interface Notification {
+  id: number;
+  message: string;
+}
+
+const notifications = ref<Notification[]>([]);
+
+const addNotification = (message: string) => {
+  const id = Date.now();
+  notifications.value.push({
+    id,
+    message,
+  });
+
+  setTimeout(() => removeNotification(id), 5000);
+};
+
+const removeNotification = (id: number) => {
+  notifications.value = notifications.value.filter((notification) => notification.id !== id);
+};
+export function useNostification() {
+  return {
+    notifications,
+    addNotification,
+    removeNotification,
+  };
+}
