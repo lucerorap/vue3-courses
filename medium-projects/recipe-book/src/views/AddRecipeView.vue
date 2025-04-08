2044f04f5f373bd97892bb3bb5a42a58ffe4f518
Adrian Rafael Lucero <adrianrafaellucero9@gmail.com>
2025-04-08 22:28:13 +0800

    Recipe Book: Programmatic Navigation

--- medium-projects/recipe-book/src/views/AddRecipeView.vue
+++ medium-projects/recipe-book/src/views/AddRecipeView.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>Add Recipe</div>
+  <div>Add Form</div>
+  <form @submit.prevent="addRecipe">
+    <button type="submit">Add</button>
+  </form>
 </template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const addRecipe = () => {
+  router.push({
+    name: 'recipe',
+    params: {
+      id: 1,
+    },
+  });
+};
+</script>
