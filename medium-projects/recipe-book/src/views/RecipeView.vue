92073af5da2717bf307963d731af7888192284fc
Adrian Rafael Lucero <adrianrafaellucero9@gmail.com>
2025-04-08 22:38:29 +0800

    Recipe Book: Data Fetching Inside the onMounted Hook Caveat & Solution

--- medium-projects/recipe-book/src/views/RecipeView.vue
+++ medium-projects/recipe-book/src/views/RecipeView.vue
@@ -1,11 +1,28 @@
 <template>
   <div>Recipe {{ $route.params.id }}</div>
+  <nav>
+    <ul>
+      <li>
+        <RouterLink :to="{ name: 'recipe', params: { id: 1 } }">Recipe 1</RouterLink>
+      </li>
+      <li>
+        <RouterLink :to="{ name: 'recipe', params: { id: 3 } }">Recipe 3</RouterLink>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
+import { onMounted, watch } from 'vue';
+import { useRoute, RouterLink } from 'vue-router';
 
 const route = useRoute();
 
 console.log(route.params.id);
+
+watch(
+  () => route.params.id,
+  () => console.log('Fetching inside watch'),
+  { immediate: true },
+);
 </script>
