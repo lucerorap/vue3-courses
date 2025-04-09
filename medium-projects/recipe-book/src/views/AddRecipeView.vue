849144e5570e11047188335b5678e333b01ebf47
Adrian Rafael Lucero <adrianrafaellucero9@gmail.com>
2025-04-09 15:13:48 +0800

    Recipe Book: Adding Actions to Pinia Store

--- medium-projects/recipe-book/src/views/AddRecipeView.vue
+++ medium-projects/recipe-book/src/views/AddRecipeView.vue
@@ -1,19 +1,36 @@
 <template>
   <div>Add Form</div>
   <form @submit.prevent="addRecipe">
+    <div>
+      <input type="text" v-model="name" placeholder="Recipe name" required />
+    </div>
+    <div>
+      <input type="text" v-model="description" placeholder="Recipe description" required />
+    </div>
     <button type="submit">Add</button>
   </form>
 </template>
 
 <script setup lang="ts">
+import { useRecipeStore } from '@/stores/recipe';
+import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
+const recipeStore = useRecipeStore();
+
+const name = ref('');
+const description = ref('');
 const router = useRouter();
 const addRecipe = () => {
+  const recipe = recipeStore.addRecipe({
+    name: name.value,
+    description: description.value,
+  });
+
   router.push({
     name: 'recipe',
     params: {
-      id: 1,
+      id: recipe.id,
     },
   });
 };
