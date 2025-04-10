15c7fb3bf16a85586b6267ccaac9ffe01b989db8
Adrian Rafael Lucero <adrianrafaellucero9@gmail.com>
2025-04-10 10:38:34 +0800

    Trello Clone: Adding the Lists UI

--- medium-projects/trello-clone/src/App.vue
+++ medium-projects/trello-clone/src/App.vue
@@ -1,5 +1,31 @@
 <template>
-  <main class="text-5xl">Helloworld</main>
+  <main class="pd-5 font-sans">
+    <div class="flex gap-5 py-5 overflow-x-auto">
+      <div class="flex flex-col bg-gray-100 p-3 rounded-lg min-w-[250px]">
+        <h2 class="font-medium mb-2">The List</h2>
+        <div class="bg-white p-2 my-2 ounded shadow-cursor-pointer">
+          <span class="text-sm font-medium">Title Card</span>
+          <p class="text-xs text-gray-400">Card Description</p>
+        </div>
+
+        <div class="bg-white p-2 my-2 ounded shadow-cursor-pointer">
+          <span class="text-sm font-medium">Title Card</span>
+          <p class="text-xs text-gray-400">Card Description</p>
+        </div>
+
+        <div class="bg-white p-2 my-2 ounded shadow-cursor-pointer">
+          <span class="text-sm font-medium">Title Card</span>
+          <p class="text-xs text-gray-400">Card Description</p>
+        </div>
+
+        <button
+          class="w-full bg-transparent rounded hover:bg-white text-gray-500 p-2 text-left mt-2 text-sm font-medium"
+        >
+          + Add Card
+        </button>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts"></script>
