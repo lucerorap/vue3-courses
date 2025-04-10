fb5540e7a8979bea437a22ff2bad51cfaaf26afb
Adrian Rafael Lucero <adrianrafaellucero9@gmail.com>
2025-04-10 10:45:39 +0800

    Trello Clone: Types & Reactive list of cards

--- medium-projects/trello-clone/src/App.vue
+++ medium-projects/trello-clone/src/App.vue
@@ -1,23 +1,60 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+interface Card {
+  id: number;
+  title: string;
+  description: string;
+}
+
+interface List {
+  id: number;
+  title: string;
+  cards: Card[];
+}
+
+const lists = reactive<List[]>([
+  {
+    id: 1,
+    title: 'To Do',
+    cards: [
+      { id: 1, title: 'Task 1', description: 'Description for Task 1' },
+      { id: 2, title: 'Task 2', description: 'Description for Task 2' },
+    ],
+  },
+  {
+    id: 2,
+    title: 'In Progress',
+    cards: [
+      { id: 3, title: 'Task 3', description: 'Description for Task 3' },
+      { id: 4, title: 'Task 4', description: 'Description for Task 4' },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Done',
+    cards: [{ id: 5, title: 'Task 5', description: 'Description for Task 5' }],
+  },
+]);
+</script>
+
 <template>
-  <main class="pd-5 font-sans">
+  <main class="p-5 font-sans">
     <div class="flex gap-5 py-5 overflow-x-auto">
-      <div class="flex flex-col bg-gray-100 p-3 rounded-lg min-w-[250px]">
-        <h2 class="font-medium mb-2">The List</h2>
-        <div class="bg-white p-2 my-2 ounded shadow-cursor-pointer">
-          <span class="text-sm font-medium">Title Card</span>
-          <p class="text-xs text-gray-400">Card Description</p>
+      <div
+        v-for="list in lists"
+        :key="list.id"
+        class="flex flex-col bg-gray-100 p-3 rounded-lg min-w-[250px]"
+      >
+        <h2 class="font-medium mb-2">{{ list.title }}</h2>
+        <div
+          v-for="card in list.cards"
+          :key="card.id"
+          class="bg-white p-2 my-2 ounded shadow-cursor-pointer"
+        >
+          <span class="text-sm font-medium">{{ card.title }}</span>
+          <p class="text-xs text-gray-400">{{ card.description }}</p>
         </div>
-
-        <div class="bg-white p-2 my-2 ounded shadow-cursor-pointer">
-          <span class="text-sm font-medium">Title Card</span>
-          <p class="text-xs text-gray-400">Card Description</p>
-        </div>
-
-        <div class="bg-white p-2 my-2 ounded shadow-cursor-pointer">
-          <span class="text-sm font-medium">Title Card</span>
-          <p class="text-xs text-gray-400">Card Description</p>
-        </div>
-
         <button
           class="w-full bg-transparent rounded hover:bg-white text-gray-500 p-2 text-left mt-2 text-sm font-medium"
         >
@@ -27,5 +64,3 @@
     </div>
   </main>
 </template>
-
-<script setup lang="ts"></script>
